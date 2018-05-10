0ccfcd222e449737d5ab7eba0392b8577fa71272
Derrek Harrison <derrek.s.harrison@gmail.com>
2018-05-09 19:50:41 -0700

    run: move run.js to root
    Move the run.js file to the root directory to line up with the “node
    run” command listed in the README.

---
+++ run.js
@@ -0,0 +1,8 @@
+/**
+ * run.js allows for local execution of apex-doc-node. The main
+ * apex-doc-node file exports the config and engine to be used
+ * in grunt or other task runners.
+ */
+
+var app = require('./apex-doc-node.js');
+app.run();
